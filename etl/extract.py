72abc7fc207347da8e626164dfd240f3e8cbafaa
Vikranth Yeddula <79628737+y-vikranth@users.noreply.github.com>
2026-04-13 16:49:50 +0530

    Delete etl directory

--- etl/extract.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-def extract(filepath: str) -> pd.DataFrame:
-    df = pd.read_csv(filepath, parse_dates=["exam_date"])
-    print(f"[Extract] Loaded {len(df)} rows from {filepath}")
-    return df
